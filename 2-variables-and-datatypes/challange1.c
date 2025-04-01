2dd7fb9f91e04936c4e731befe414d4130e058e3
Waleed Tariq <waleedtariq109@outlook.com>
2025-04-02 02:33:34 +0500

    challange 1

--- 2-variables-and-datatypes/challange1.c
+++ 2-variables-and-datatypes/challange1.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  return 0;
+  int grade1, grade2, grade3;
+  double result;
+  printf("%s", "Enter Grade1: ");
+  scanf("%d", &grade1);
+  printf("%s", "Enter Grade2: ");
+  scanf("%d", &grade2);
+  printf("%s", "Enter Grade3: ");
+  scanf("%d", &grade3);
+  result = ((double)grade1 + (double)grade2 + (double)grade3) / 3;
+  printf("%s %.2lf%%\n", "The total percentage of 3 grades is:", result);
 }
