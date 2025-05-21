f5607d12e1d1d620bdd900a09d9bea4c9a0c32c0
Waleed Tariq <waleedtariq109@outlook.com>
2025-05-21 18:51:19 +0500

    rm rf ./*

--- v1/0-first-program/challange1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  // Print your name
-  printf("First name: Waleed \nLast name: Tariq\n");
-  return 0;
-}
--- v1/0-first-program/challange2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  // Print your name, age and gender
-  printf("Name: Waleed Tariq \nAge: 23 \nGender: Male\n");
-  return 0;
-}
