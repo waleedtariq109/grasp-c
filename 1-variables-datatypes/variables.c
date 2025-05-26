db81ffb9674bd015ee4f118e5eccf8c4abb7e7d9
Waleed Tariq <waleedtariq109@outlook.com>
2025-05-27 02:02:15 +0500

    variables

---
+++ 1-variables-datatypes/variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int num;   // Initialize a variable
+  num = 10;  // Assigning a value
+  printf("num is %d\n", num);
+
+  scanf("%d", &num);
+  printf("num is %d\n", num);
+
+  return 0;
+}
