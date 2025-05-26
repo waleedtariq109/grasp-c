f726946c34a83985b10c3dfb6e4ac1deb6d5d32b
Waleed Tariq <waleedtariq109@outlook.com>
2025-05-26 23:18:58 +0500

    defining function

--- 5-functions/defining-functon.c
+++ 5-functions/defining-functon.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/**
+ * If we define the function prototype at the top and call the function before
+ * its definition, then this is valid C code.
+ *
+ * If we don't define the function prototype, then we must declare the function
+ * before calling it.
+ */
+
+void add();  // -> Function prototype
+
 int main() {
+  add();
   return 0;
+}
+
+void add() {
 }
